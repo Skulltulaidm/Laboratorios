149a2774723fa713e2fa5acecb03fa47eea253eb
Luis Alberto Rodríguez Solís <skulltula@Luiss-MacBook-Air.local>
2025-02-17 21:11:34 -0600

    mover a lab1'

--- src/data/airports.js
+++
@@ -1,7 +0,0 @@
-const airports = [
-    { id: 1, name: "Aeropuerto Internacional de Los Ángeles", code: "LAX" },
-    { id: 2, name: "Aeropuerto Internacional de Miami", code: "MIA" },
-    { id: 3, name: "Aeropuerto Internacional de Madrid-Barajas", code: "MAD" }
-];
-
-export default airports;
